06dadbcfcac0b3ed9cd60b38c1f6751fa6db6e2e
Jonathon Rudy <jrudy2@mail.csuchico.edu>
2019-04-23 12:59:05 -0700

    Adding in additional unit test

--- src/test/java/io/liatr/sampleappapi/PostTest.java
+++ src/test/java/io/liatr/sampleappapi/PostTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @ExtendWith(SpringExtension.class)
 public class PostTest {
@@ -15,11 +15,64 @@ public class PostTest {
     String link = "devops.com";
 
     @Test
+    public void toStringTest() throws Exception {
+        Post hc = new Post();
+        hc.setFirstName(name);
+        assertEquals("Post(id=null, firstName=jon, title=null, link=null)", hc.toString());
+    }
+
+    @Test
+    public void toStringTitleTest() throws Exception {
+        Post hc = new Post();
+        hc.setTitle(title);
+        hc.setFirstName(name);
+        assertEquals("Post(id=null, firstName=jon, title=devops, link=null)", hc.toString());
+    }
+
+    @Test
+    public void getClassTest() throws Exception {
+        Post hc = new Post();
+        hc.setTitle(title);
+        hc.getTitle().hashCode();
+        
+        assertEquals("class io.liatr.sampleappapi.Post", hc.getClass().toString());
+    }
+
+
+
+    @Test
+    public void equalsTest() throws Exception {
+        Post hc = new Post();
+        hc.setFirstName(name);
+        Post hc2 = new Post();
+        hc2.setFirstName(name);
+        assertTrue(hc.equals(hc2));
+    }
+
+    @Test
+    public void hashCodeTest() throws Exception {
+        Post hc = new Post();
+        hc.setFirstName(name);
+        Post hc2 = new Post();
+        hc2.setFirstName(name);
+        assertEquals(hc.hashCode(), hc2.hashCode());
+    }
+
+    @Test
     public void getIdTest() throws Exception {
         Post hc = new Post();
         hc.setId(id);
         Long test = hc.getId();
         assertEquals(id, test);
+    }
+
+    @Test
+    public void getBadIdTest() throws Exception {
+        Post hc = new Post();
+        hc.setId(id);
+        Long test = hc.getId();
+        Long badVal = 12345678912L;
+        assertNotEquals(badVal, test);
     }
 
     @Test
